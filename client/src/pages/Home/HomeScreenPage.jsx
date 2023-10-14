a7348d0f529b6ba547361a906cf02ea5adae0415
farrez30 <59773413+farrez30@users.noreply.github.com>
2023-10-14 23:59:51 +0700

    Update HomeScreenPage.jsx

--- client/src/pages/Home/HomeScreenPage.jsx
+++ client/src/pages/Home/HomeScreenPage.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import TopNavbar from "../../components/TopNavbar/TopNavbar";
 import DashboardGreeting from "../../components/Dashboard/DashboardGreeting";
+import DashboardMenu from "../../components/Dashboard/DashboardMenu";
+import DashboardBotOverview from "../../components/Dashboard/DashboardBotOverview";
 
 const HomePage = () => {
   return (
@@ -9,6 +11,8 @@ const HomePage = () => {
         {/* <h1 className="text-3xl font-bold underline bg-red-600">Home</h1> */}
         <TopNavbar />
         <DashboardGreeting />
+        <DashboardMenu />
+        <DashboardBotOverview />
         {/* <img src={require("../../../public/assets/icons/setting.svg")} alt="settings" /> */}
       </div>
     </>
