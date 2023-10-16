06ce8b044a6508bbb936a7023368c0df77ab583f
farrez30 <59773413+farrez30@users.noreply.github.com>
2023-10-17 06:03:36 +0700

    adding AboutUs page

--- client/src/pages/AboutUs/AboutUsPage.jsx
+++ client/src/pages/AboutUs/AboutUsPage.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { ReactComponent as LeftArrowIcon } from "../../icons/left-arrow.svg";
+import AboutUsContent from "../../components/AboutUsContent/AboutUsContent";
 
 const AboutUsPage = () => {
-  return (
-    <div>AboutUsPage</div>
-  )
-}
+  const navigate = useNavigate();
 
-export default AboutUsPage
+  return (
+    <>
+      <div className="bg-cuanbot-light h-screen">
+        <div className="bg-cuanbot-light flex justify-between pt-10 px-[10px] pb-5">
+          <button
+            onClick={() => navigate(-1)}
+            className="text-cuanbot-light-gray font-semibold text-lg flex items-center"
+          >
+            <LeftArrowIcon className="mr-[10px]" />
+            <h1>Settings</h1>
+          </button>
+        </div>
+        <AboutUsContent />
+      </div>
+    </>
+  );
+};
+
+export default AboutUsPage;
